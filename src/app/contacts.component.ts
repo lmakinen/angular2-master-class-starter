5fe6eafd98ba187e33e277d390cdb4396b19c1f7
Lasse Mäkinen <lasse.makinen@frantic.com>
2016-09-05 11:55:32 +0300

    loop contacts

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
-import { Contact } from './models/Contact'
+import { Contact } from './models/contact'
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -9,21 +10,6 @@ import { Contact } from './models/Contact'
   directives: [ContactsHeaderComponent]
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
 
-  contact: Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/7.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  } 
+  contacts: Contact[] = CONTACT_DATA;
 }
