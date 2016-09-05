01a7f22e8d05be2bc7a420f8716e3823184e53a7
Lasse Mäkinen <lasse.makinen@frantic.com>
2016-09-05 11:17:05 +0300

    first contact

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ContactsHeaderComponent } from './contacts-header';
+import { Contact } from './models/Contact'
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
-  styleUrls: ['contacts.component.css']
+  styleUrls: ['contacts.component.css'],
+  directives: [ContactsHeaderComponent]
 })
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
+
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/7.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  } 
 }
